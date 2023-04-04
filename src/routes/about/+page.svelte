33d69e81280da7f7cc80ab8363ccd8c9c1ddf618
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-05 00:55:41 +0800

    Create +page.svelte

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,48 @@
+<ul>
+	<div class="container">
+		<h1>It's still under constuction. But here are some of my links</h1>
+		<div class="list">
+			<li>
+				<a href="https://www.linkedin.com/in/whoong-zi-wei/">LinkedIn</a>
+			</li>
+			<li>
+				<a href="https://github.com/ziwei531">Github</a>
+			</li>
+		</div>
+	</div>
+</ul>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	ul {
+		list-style-type: none;
+	}
+
+	.list {
+		display: flex;
+		gap: 1rem;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.list li a {
+		font-size: 1.5rem;
+		font-weight: 700;
+		color: #0ea5e9;
+		text-decoration: none;
+	}
+
+	.list li a:hover {
+		color: #0369a1;
+		transition: all;
+		transition-duration: 0.3s;
+		transition-timing-function: ease-out;
+	}
+</style>
