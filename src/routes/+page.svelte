55967848d41bbf97b175b0e1d476353d0f56bd17
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-05 16:22:51 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,59 @@
+<script>
+	import Skills from "$lib/components/Skills.svelte";
+	import Frameworks from "$lib/components/Frameworks.svelte";
+</script>
 
-<article class="container"> 
-    <h1>Welcome to Whoong Zi Wei's Website!</h1>
-    <h2>This site is still under construction</h2>
+<article class="main-container">
+	<div class="welcome-container">
+		<h1>Welcome to Whoong Zi Wei's Website!</h1>
+		<h2>I am computer science student aspiring to become a web developer</h2>
+	</div>
+	<img src="/images/profile-picture.jpg" alt="zi wei profile pic" />
 </article>
 
-<style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
+<Skills />
+<Frameworks />
 
-    h1 {
-        font-size: 3rem;
-        font-weight: 700;
-        color: #000;
-    }
+<style>
+	.main-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		gap: 1.5rem;
+	}
+
+	.main-container > img {
+		width: 300px;
+		height: 300px;
+		border-radius: 50%;
+		margin-right: 2rem;
+		object-fit: cover;
+	}
+
+	.welcome-container {
+		text-align: center;
+		padding: 1rem;
+	}
+
+	h1 {
+		font-size: 3rem;
+		font-weight: 700;
+		color: #000;
+		text-align: center;
+	}
+
+	@media only screen and (max-width: 768px) {
+		.main-container {
+			flex-direction: column;
+		}
+
+		.main-container h1 {
+			font-size: 2rem;
+		}
+
+		.main-container h2 {
+			font-size: 1rem;
+		}
+	}
 </style>
