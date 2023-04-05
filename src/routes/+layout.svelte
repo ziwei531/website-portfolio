08e42a7f4fc1701a42892e8a275c46bbcbd64eb2
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-05 16:22:48 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,48 @@
 <nav>
-    <a href="/">Home</a>
-    <a href="/projects">Projects</a>
-    <a href="/about">About</a>
+	<a href="/">Home</a>
+	<a href="/projects">Projects</a>
+	<a href="/about">About</a>
 </nav>
 
-<slot></slot>
+<slot />
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap");
+	@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap");
 
-    :root {
-        font-family: Poppins, Arial, Helvetica, sans-serif;
-        background-color: #ebf8ff;
-    }
+	:root {
+		font-family: Poppins, Arial, Helvetica, sans-serif;
+		--brand-100: #ebf8ff;
+		--brand-200: #0ea5e9;
+		--brand-300: #7dd3fc;
+		--brand-400: #075985;
+		background-color: var(--brand-100);
+	}
 
-    :global(body) {
-        box-sizing: border-box;
-        margin: 0; 
-        padding: 0;
-    }
+	:global(body) {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
 
-    nav {
-        display: flex;
-        justify-content: flex-end;
-        gap: 1rem;
-        align-items: center;
-        padding: 1rem;
-        background: #0369a1;
-        font-weight: 900;
-    }
+	nav {
+		display: flex;
+		justify-content: flex-end;
+		gap: 1rem;
+		align-items: center;
+		padding: 1rem;
+		background: #0369a1;
+		font-weight: 900;
+	}
 
-    nav a {
-        color: #fff;
-        text-decoration: none;
-        cursor: pointer;
-    }
+	nav a {
+		color: #fff;
+		text-decoration: none;
+		cursor: pointer;
+	}
 
-    nav a:hover {
-        color: #7dd3fc;
-        transition: all; 
-        transition-duration: 0.6s;
-
-    }
-
+	nav a:hover {
+		color: #7dd3fc;
+		transition: all;
+		transition-duration: 0.6s;
+	}
 </style>
