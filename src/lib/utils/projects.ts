b9ee1244d6e33444ebd082e56ccd80725fd4bee9
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-05 12:53:36 +0800

    add new projects

--- src/lib/utils/projects.ts
+++ src/lib/utils/projects.ts
@@ -16,6 +16,20 @@ let projects: Project[] = [
 		demo: "https://anime-list-website.vercel.app/",
 	},
 	{
+		id: 4,
+		name: "Full Stack To Do List Application",
+		img: "https://i.imgur.com/Ymi7ohv.png",
+		github: "https://github.com/ziwei531/to-do-list",
+		demo: "https://to-do-list-ziwei531.vercel.app/",
+	},
+	{
+		id: 5,
+		name: "URL Shortener",
+		img: "https://i.imgur.com/cQoBZyc.png",
+		github: "https://github.com/ziwei531/URL-shortener",
+		demo: "https://kind-ruby-bee-coat.cyclic.app",
+	},
+	{
 		id: 2,
 		name: "Next JS Blog",
 		img: "https://i.imgur.com/o7Df8Um.png",
@@ -28,6 +42,42 @@ let projects: Project[] = [
 		img: "https://i.imgur.com/iDssqbP.png",
 		github: "https://github.com/ziwei531/cyclistic-analysis",
 		demo: "https://wzw-portfolio.netlify.app/case_study",
+	},
+
+	{
+		id: 6,
+		name: "Documentation Website",
+		img: "https://i.imgur.com/pF77DjX.png",
+		github: "https://github.com/ziwei531/fcc-js-docs",
+		demo: "https://ziwei531.github.io/fcc-js-docs",
+	},
+	{
+		id: 7,
+		name: "Product Landing Page",
+		img: "https://i.imgur.com/6pFLmpH.png",
+		github: "https://github.com/ziwei531/fcc-product-landing-page",
+		demo: "https://ziwei531.github.io/fcc-product-landing-page/",
+	},
+	{
+		id: 8,
+		name: "Sample Portfolio Website",
+		img: "https://i.imgur.com/yzWVbaG.png",
+		github: "https://github.com/ziwei531/fcc-personal-portfolio",
+		demo: "https://ziwei531.github.io/fcc-personal-portfolio/",
+	},
+	{
+		id: 9,
+		name: "Sample Survey Form",
+		img: "https://i.imgur.com/kPFY7DQ.png",
+		github: "https://github.com/ziwei531/fcc-survey-form",
+		demo: "https://ziwei531.github.io/fcc-survey-form/",
+	},
+	{
+		id: 10,
+		name: "Tribute Website",
+		img: "https://i.imgur.com/umCKSf6.png",
+		github: "https://github.com/ziwei531/fcc-tribute-website",
+		demo: "https://ziwei531.github.io/fcc-tribute-page/",
 	},
 ];
 
