5057f0f83b97ca8303deb041ab16d73e517e0ab3
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-05 20:32:48 +0800

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,59 +1,89 @@
-<ul>
-	<div class="container">
-		<h1>It's still under constuction. But here are some of my links</h1>
-		<div class="list">
-			<li>
-				<a href="https://www.linkedin.com/in/whoong-zi-wei/">LinkedIn</a>
-			</li>
-			<li>
-				<a href="https://github.com/ziwei531">Github</a>
-			</li>
+<div class="container">
+	<div class="about-container">
+		<div class="header">
+			<h1>About me 👨‍💻</h1>
+		</div>
+
+		<p>
+			I am a computer science student in Taylor's University. Aside from the
+			syllabus that all of us CS students typically take in my university, I
+			have taken upon myself to self-study quite a lot so as to stay relevant
+			with what the industry may be seeking. Historically, I have studied and
+			taken courses that enhanced my data science skills and also web
+			development skills, which have greatly benefited in me further
+			understanding the fundamentals of programming in general.
+		</p>
+
+		<p>
+			If you are interested in giving me a chance to work for you, please don't
+			hesitate to contact me through the contact details below!
+		</p>
+
+		<div class="contact">
+			<h2>Contact me 📧</h2>
+			<div class="row">
+				<p>Phone Number: 010-9315538</p>
+				<p>
+					Email: <a target="_blank" href="mailto:whoong.ziwei@gmail.com"
+						>whoong.ziwei@gmail.com</a
+					>
+				</p>
+				<p><a href="https://www.linkedin.com/in/whoong-zi-wei/">Linkedin</a></p>
+				<p><a href="https://github.com/ziwei531">Github</a></p>
+			</div>
 		</div>
 	</div>
-</ul>
+</div>
 
 <style>
 	.container {
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
 		align-items: center;
-		padding: 1rem;
+		justify-content: center;
 		height: 100vh;
 	}
 
-	ul {
-		list-style-type: none;
+	.about-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		padding: 2rem;
+		border-radius: 0.5rem;
+		max-width: 800px;
+		background-color: white;
+		box-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
 	}
 
-	.list {
-		display: flex;
+	.container h2 {
+		text-align: center;
+	}
+
+	.contact {
+		text-align: center;
+	}
+	.row {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
 		gap: 1rem;
 		justify-content: center;
-		align-items: center;
 	}
 
-	.list li a {
-		font-size: 1.5rem;
-		font-weight: 700;
-		color: #0ea5e9;
+	.row a {
+		color: var(--brand-400);
 		text-decoration: none;
 	}
 
-	.list li a:hover {
-		color: #0369a1;
+	.row a:hover {
+		color: var(--brand-300);
 		transition: all;
 		transition-duration: 0.3s;
-		transition-timing-function: ease-out;
+		transition-timing-function: ease-in-out;
 	}
 
 	@media only screen and (max-width: 768px) {
-		.container h1 {
-			font-size: 1.5rem;
-			text-align: center;
-		}
-		.list li a {
-			font-size: 1rem;
+		.container {
+			padding: 2rem;
 		}
 	}
 </style>
