41f1d48081dae20874ec569c80dde19d1e98ea6e
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-19 23:43:07 +0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,6 +35,21 @@
 		name="keywords"
 		content="wzw, Whoong Zi Wei, Zi Wei, Whoong, Taylor's University, WZW"
 	/>
+
+	<!-- Google tag (gtag.js) -->
+	<script
+		async
+		src="https://www.googletagmanager.com/gtag/js?id=G-M5E9C42ZR3"
+	></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag("js", new Date());
+
+		gtag("config", "G-M5E9C42ZR3");
+	</script>
 </svelte:head>
 
 <!--Hamburger menu import-->
