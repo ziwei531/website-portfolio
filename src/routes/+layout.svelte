64f83a83d66a98dbbf94b1f0c46130f67561e902
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2024-02-26 13:40:59 +0800

    update resume

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,217 +1,216 @@
 <script>
-	import { onMount } from "svelte";
-	import { writable } from "svelte/store";
+    import { onMount } from "svelte";
+    import { writable } from "svelte/store";
 
-	const isMobile = writable(false);
+    const isMobile = writable(false);
 
-	onMount(() => {
-		// set the initial value of isMobile based on the screen width
-		const handleResize = () => {
-			isMobile.set(window.innerWidth < 768);
-		};
+    onMount(() => {
+        // set the initial value of isMobile based on the screen width
+        const handleResize = () => {
+            isMobile.set(window.innerWidth < 768);
+        };
 
-		handleResize();
+        handleResize();
 
-		window.addEventListener("resize", handleResize);
+        window.addEventListener("resize", handleResize);
 
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	});
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    });
 
-	const now = new Date();
-	const year = now.getFullYear();
+    const now = new Date();
+    const year = now.getFullYear();
 
-	let toggle = false; // false = closed, true = open
-	function handleToggle() {
-		toggle = !toggle;
-	}
+    let toggle = false; // false = closed, true = open
+    function handleToggle() {
+        toggle = !toggle;
+    }
+
+    const resumeLink = "https://ziwei531.github.io/resumePdf-hosting/";
+
+    // export the store
+    export { isMobile, year, handleToggle, toggle, resumeLink };
 </script>
 
 <svelte:head>
-	<title>Whoong Zi Wei</title>
-	<meta name="description" content="Whoong Zi Wei's personal website" />
-	<meta
-		name="keywords"
-		content="wzw, Whoong Zi Wei, Zi Wei, Whoong, Taylor's University, WZW"
-	/>
+    <title>Whoong Zi Wei</title>
+    <meta name="description" content="Whoong Zi Wei's personal website" />
+    <meta
+        name="keywords"
+        content="wzw, Whoong Zi Wei, Zi Wei, Whoong, Taylor's University, WZW"
+    />
 
-	<!-- Google tag (gtag.js) -->
-	<!-- Google tag (gtag.js) -->
-	<script
-		async
-		src="https://www.googletagmanager.com/gtag/js?id=G-M5E9C42ZR3"
-	></script>
-	<script>
-		try {
-			if (typeof window !== "undefined" && window) {
-				window.dataLayer = window.dataLayer || [];
-				function gtag() {
-					dataLayer.push(arguments);
-				}
-				gtag("js", new Date());
+    <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-M5E9C42ZR3"
+    ></script>
+    <script>
+        try {
+            if (typeof window !== "undefined" && window) {
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
+                gtag("js", new Date());
 
-				gtag("config", "G-M5E9C42ZR3");
-			}
-		} catch (error) {
-			console.log("Failed to load Google Analytics");
-		}
-	</script>
+                gtag("config", "G-M5E9C42ZR3");
+            }
+        } catch (error) {
+            console.log("Failed to load Google Analytics");
+        }
+    </script>
 </svelte:head>
 
 <!--Hamburger menu import-->
 <link
-	rel="stylesheet"
-	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
 />
 
 <nav>
-	<div class="logo">
-		<a href="/">Whoong Zi Wei</a>
-	</div>
+    <div class="logo">
+        <a href="/">Whoong Zi Wei</a>
+    </div>
 
-	{#if toggle && $isMobile}
-		<a on:click={handleToggle} href="/">Home</a>
-		<a on:click={handleToggle} href="/projects">Projects</a>
-		<a on:click={handleToggle} href="/about">About</a>
-		<a
-			on:click={handleToggle}
-			target="_blank"
-			href="https://flowcv.com/resume/qsrj03pfkd">Resume</a
-		>
-	{/if}
+    {#if toggle && $isMobile}
+        <a on:click={handleToggle} href="/">Home</a>
+        <a on:click={handleToggle} href="/projects">Projects</a>
+        <a on:click={handleToggle} href="/about">About</a>
+        <a target="_blank" href={resumeLink}>Resume</a>
+    {/if}
 
-	{#if !$isMobile}
-		<a href="/">Home</a>
-		<a href="/projects">Projects</a>
-		<a href="/about">About</a>
-		<a target="_blank" href="https://flowcv.com/resume/qsrj03pfkd">Resume</a>
-	{/if}
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<i on:click={handleToggle} class="menu-toggler fa fa-bars" />
+    {#if !$isMobile}
+        <a href="/">Home</a>
+        <a href="/projects">Projects</a>
+        <a href="/about">About</a>
+        <a target="_blank" href={resumeLink}>Resume</a>
+    {/if}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <i on:click={handleToggle} class="menu-toggler fa fa-bars" />
 </nav>
 
 <slot />
 
 <footer>
-	<p>© {year} Whoong Zi Wei</p>
-	<p>
-		Thank you for viewing my website! This <a
-			target="_blank"
-			href="https://github.com/ziwei531/internship-portfolio">website</a
-		> is created with Sveltekit 😎
-	</p>
+    <p>© {year} Whoong Zi Wei</p>
+    <p>
+        Thank you for viewing my website! This <a
+            target="_blank"
+            href="https://github.com/ziwei531/internship-portfolio">website</a
+        > is created with Sveltekit 😎
+    </p>
 </footer>
 
 <style>
-	@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap");
 
-	:root {
-		font-family: Poppins, Arial, Helvetica, sans-serif;
-		--brand-100: #ebf8ff;
-		--brand-200: #0ea5e9;
-		--brand-300: #7dd3fc;
-		--brand-400: #075985;
-		background-color: var(--brand-100);
-	}
+    :root {
+        font-family: Poppins, Arial, Helvetica, sans-serif;
+        --brand-100: #ebf8ff;
+        --brand-200: #0ea5e9;
+        --brand-300: #7dd3fc;
+        --brand-400: #075985;
+        background-color: var(--brand-100);
+    }
 
-	:global(body) {
-		box-sizing: border-box;
-		margin: 0;
-		padding: 0;
-	}
+    :global(body) {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-	.menu-toggler {
-		display: none;
-		width: 1.5rem;
-		height: 1.5rem;
-		cursor: pointer;
-	}
+    .menu-toggler {
+        display: none;
+        width: 1.5rem;
+        height: 1.5rem;
+        cursor: pointer;
+    }
 
-	nav {
-		display: flex;
-		justify-content: flex-end;
-		gap: 1rem;
-		padding: 1rem;
-		background: #0369a1;
-		font-weight: 900;
-		position: sticky;
-		top: 0;
-		z-index: 1;
-	}
+    nav {
+        display: flex;
+        justify-content: flex-end;
+        gap: 1rem;
+        padding: 1rem;
+        background: #0369a1;
+        font-weight: 900;
+        position: sticky;
+        top: 0;
+        z-index: 1;
+    }
 
-	nav a {
-		color: #fff;
-		text-decoration: none;
-		cursor: pointer;
-		padding-right: 30px;
-	}
+    nav a {
+        color: #fff;
+        text-decoration: none;
+        cursor: pointer;
+        padding-right: 30px;
+    }
 
-	nav .logo {
-		margin-right: auto;
-	}
+    nav .logo {
+        margin-right: auto;
+    }
 
-	nav a:hover {
-		color: #7dd3fc;
-		transition: all;
-		transition-duration: 0.6s;
-	}
+    nav a:hover {
+        color: #7dd3fc;
+        transition: all;
+        transition-duration: 0.6s;
+    }
 
-	footer {
-		padding: 1rem;
-		text-align: center;
-		background: var(--brand-400);
-	}
+    footer {
+        padding: 1rem;
+        text-align: center;
+        background: var(--brand-400);
+    }
 
-	footer p {
-		color: #fff;
-		font-weight: 900;
-		font-size: 1.5rem;
-	}
+    footer p {
+        color: #fff;
+        font-weight: 900;
+        font-size: 1.5rem;
+    }
 
-	footer a {
-		color: var(--brand-300);
-		text-decoration: none;
-	}
+    footer a {
+        color: var(--brand-300);
+        text-decoration: none;
+    }
 
-	footer a:hover {
-		color: var(--brand-200);
-		transition: all;
-		transition-duration: 0.3s;
-		transition-timing-function: ease-in-out;
-	}
+    footer a:hover {
+        color: var(--brand-200);
+        transition: all;
+        transition-duration: 0.3s;
+        transition-timing-function: ease-in-out;
+    }
 
-	@media only screen and (max-width: 768px) {
-		footer p {
-			font-size: 1rem;
-		}
+    @media only screen and (max-width: 768px) {
+        footer p {
+            font-size: 1rem;
+        }
 
-		nav {
-			justify-content: center;
-			align-items: center;
-		}
+        nav {
+            justify-content: center;
+            align-items: center;
+        }
 
-		nav a {
-			padding-right: 1rem;
-			padding-left: 1rem;
-		}
-		/* 
+        nav a {
+            padding-right: 1rem;
+            padding-left: 1rem;
+        }
+        /* 
 			nav .logo {
 				margin-right: 0;
 			} 
 			this will make it centered
 		*/
 
-		nav {
-			flex-direction: column;
-		}
+        nav {
+            flex-direction: column;
+        }
 
-		.menu-toggler {
-			position: absolute;
-			top: 1rem;
-			right: 1rem;
-			display: block;
-			color: #ebf8ff;
-		}
-	}
+        .menu-toggler {
+            position: absolute;
+            top: 1rem;
+            right: 1rem;
+            display: block;
+            color: #ebf8ff;
+        }
+    }
 </style>
